f2873ef7f159a036b21ddceb8387a6238e687cb3
Eseosa Travis Eweka <traviseweka39@gmail.com>
2026-03-13 16:38:28 -0400

    refactor: modified the fee table for new property in service layer

--- Hospital-Management-System/Models/Fee.cs
+++ Hospital-Management-System/Models/Fee.cs
@@ -14,25 +14,54 @@ public partial class Fee
     [Column("FeeID")]
     public int FeeId { get; set; }
 
+    
+    
+    
     [Column("PatientID")]
     public int? PatientId { get; set; }
 
+    
+    
+
+    [Column("PatientName")]
+    [StringLength(100)]
+    public string PatientName { get; set; } = null!; 
+
+    
+    
     [Column("DoctorID")]
     public int? DoctorId { get; set; }
 
+    
+    
+    
     [Column(TypeName = "datetime")]
     public DateTime? FeeDate { get; set; }
 
+    
+    
     [StringLength(100)]
     public string ServiceName { get; set; } = null!;
 
+
+    public bool IsPaid { get; set; } = false; 
+    
+    
+    public DateTime LastModified { get; set; } = DateTime.UtcNow; 
+    
+    
+    
     [Precision(10, 2)]
     public decimal Amount { get; set; }
 
+    
+    
     [ForeignKey("DoctorId")]
     [InverseProperty("Fees")]
     public virtual Doctor? Doctor { get; set; }
 
+    
+    
     [ForeignKey("PatientId")]
     [InverseProperty("Fees")]
     public virtual Patient? Patient { get; set; }
